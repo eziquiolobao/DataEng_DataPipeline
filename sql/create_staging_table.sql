6a6836850c3233e3689219211d9c223bfafbec81
Eziquio <eziquio.lobao@gmail.com>
2025-07-09 15:19:34 -0400

    Changed column names in fact and dim

--- sql/create_staging_table.sql
+++ sql/create_staging_table.sql
@@ -2,14 +2,17 @@
 CREATE TABLE IF NOT EXISTS stg_redfin_sales (
     address TEXT,
     city TEXT,
-    state TEXT,
     zip_code TEXT,
+    state_or_province TEXT,
+    property_type TEXT,
     price NUMERIC,
     beds INTEGER,
     baths NUMERIC,
     square_feet INTEGER,
     sold_date DATE,
     property_type TEXT,
+    lot_size NUMERIC,
+    year_built INTEGER,
     latitude NUMERIC,
     longitude NUMERIC
 );
